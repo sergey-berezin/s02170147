98a3536f2a927e995c68a8bc25a24d5683e05fee
Drobashique <65277374+Drobashique@users.noreply.github.com>
2020-10-12 10:59:09 +0300

    Add files via upload

--- ImageProcessor/onnxModel/Onnx.cs
+++ ImageProcessor/onnxModel/Onnx.cs
@@ -5,6 +5,7 @@ using System.Threading;
 using SixLabors.ImageSharp; // Из одноимённого пакета NuGet
 using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
+using System.Collections.Concurrent;
 using Microsoft.ML.OnnxRuntime.Tensors;
 using Microsoft.ML.OnnxRuntime;
 using System.Collections.Generic;
@@ -13,25 +14,15 @@ namespace onnxModel
 {
     public class OnnxModel
     {
-
-        string path, inputname;
+        private string path;
+        private ConcurrentQueue<string> _filenames;
         InferenceSession session;
+        private static readonly ManualResetEvent StopSignal = new ManualResetEvent(false);
 
-        public string Path
-        {
-            get {return path;}
-            set {path = value;}
-        }
-        public string InputName
-        {
-            get {return inputname;}
-            set {inputname = value;}
-        }
-        public OnnxModel(string Path="mnist-8.onnx", string InputName="Input3")
+        public OnnxModel(string Path = "Users/vladimirlisovoi/desktop/prak1/")
         {
             this.path = Path;
-            this.inputname = InputName;
-            this.session = new InferenceSession(path);
+            this.session = new InferenceSession("/Users/vladimirlisovoi/desktop/prak1/ImageProcessor/onnxModel/mnist-8.onnx");
         }
 
         public object FileRead(string ImagePath)
@@ -56,83 +47,95 @@ namespace onnxModel
                 for (int x = 0; x < TargetWidth; x++)
                     input[0, 0, y, x] = image[x,y].R / 255f;
 
-            var inputs = new List<NamedOnnxValue>  
-            { 
-                NamedOnnxValue.CreateFromTensor("Input3", input) 
-            };
-
+            var inputs = new List<NamedOnnxValue> {NamedOnnxValue.CreateFromTensor("Input3", input)};
             using IDisposableReadOnlyCollection<DisposableNamedOnnxValue> results = session.Run(inputs);
-            var output = results.First().AsEnumerable<float>().ToArray();
+
+            var output = results.First().AsEnumerable<float>();
             var sum = output.Sum(x => (float)Math.Exp(x));
             var softmax = output.Select(x => (float)Math.Exp(x) / sum);
 
-            float maxsoftmax = softmax.Max();
-            var l = softmax.ToList();
-            int maxsoftmaxindex = l.IndexOf(maxsoftmax);
-
-            return (maxsoftmaxindex, maxsoftmax);
-
-        }
-
-    }
-
-    public delegate void OutputHandler(object sender, params object[] args);
-    public class AsyncProcessor
-    {
-        static CancellationTokenSource cancel = new CancellationTokenSource();
-
-        OnnxModel model;
-        public event OutputHandler DataEvent;
-
-        public AsyncProcessor(OnnxModel model)
-        {
-            this.model = model;
-            Console.CancelKeyPress += new ConsoleCancelEventHandler((s, args)=>{args.Cancel = true; cancel.Cancel();});
-        }
-
-        public void Worker(string DirectoryPath)
-        {
-
-            cancel = new CancellationTokenSource();
-
-            string[] Files = Directory.GetFiles(DirectoryPath);
-
-            Thread[] threads = new Thread[Environment.ProcessorCount];
-
-            int processed = -1;
-
-            for (int i = 0; i < Environment.ProcessorCount; i++)
+            var preds = softmax.Select((x, i) =>
+                    new Tuple<string, float>(LabelMap.ClassLabels[i], x))
+                    .OrderByDescending(x => x.Item2)
+                    .Take(10);
+            var prediction = "\n";
+            foreach (var (label, confidence) in preds.ToList())
             {
-                threads[i] = new Thread(()=>
-                {
-                    int Numb;
+                prediction += $"Label: {label}, confidence: {confidence}\n";
+            }
+            return prediction;
+        
+        }
 
-                    while(!cancel.Token.IsCancellationRequested)
-                    {
-
-                        Numb = Interlocked.Increment(ref processed);
-                        if(Numb >= Files.Count())
-                            break;
-                        else
-                        {
-                            object output = model.FileRead(Files[Numb]);
-                            DataEvent?.Invoke(this, Files[Numb], output);
-
-                        }
-                    }
-
-
-                });
+        public void Work()
+        {
+            try
+            {
+                _filenames = new ConcurrentQueue<string>(Directory.GetFiles(path, "*.jpg"));
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("No files");
+                return;
+            }
+            Console.CancelKeyPress += (sender, eArgs) =>
+            {
+                StopSignal.Set();
+                eArgs.Cancel = true;
+            };
+            var procNumb = Environment.ProcessorCount;
+            var threads = new Thread[procNumb];
+            for (var i = 0; i < procNumb; ++i)
+            {
+                Console.WriteLine($"Starting thread {i}");
+                threads[i] = new Thread(Worker);
                 threads[i].Start();
             }
 
-             for (int i = 0; i < Environment.ProcessorCount; i++)
+            for (var i = 0; i < procNumb; ++i)
             {
                 threads[i].Join();
             }
 
+            Console.WriteLine("Success");
         }
 
+        private void Worker()
+        {
+            while (_filenames.TryDequeue(out var name))
+            {
+                if (StopSignal.WaitOne(0))
+                {
+                    Console.WriteLine("Finishing thread");
+                    return;
+                }
+
+                var prediction = FileRead(name);
+                Console.WriteLine(name + prediction);
+
+            }
+
+            Console.WriteLine("Thread finished");
+        }
+
+    }
+
+
+    static class LabelMap
+    {
+        public static readonly string[] ClassLabels = new[]
+        {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+        };
     }
 
 } 
